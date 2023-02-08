400f756bb3dee318cf0270125f177cd3dbc17c54
thatavieira <thagurgel0909@gmail.com>
2023-02-08 16:00:53 -0300

    project stopwatch

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start(6);
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length -1,1));
+            Console.WriteLine(data);
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);    
+            }    
+            
+            PreStart(time * multiplier);
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...?");
+            Thread.Sleep(2500);
+            
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currenTime = 0;
+            
+            while (currenTime != time)
+            {
+                Console.Clear();
+                currenTime++;
+                Console.WriteLine(currenTime);
+                Thread.Sleep(1000);
+            }
+            
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
