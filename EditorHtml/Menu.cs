cbd7776a956c8b660ff0e18a320e78cdbfaf7455
thatavieira <thagurgel0909@gmail.com>
2023-02-23 18:12:52 -0300

    wip: project EditorHtml

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,13 @@
+namespace EditorHtml;
+
+public class Menu
+{
+    public static void Show()
+    {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.Black;
+        
+        
+    }
+}
---
+++ EditorHtml/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EditorHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
+
