138daf9ea0f24c4b877a8f543c4017ba8dfbf3f2
thatavieira <thagurgel0909@gmail.com>
2023-03-24 13:24:12 -0300

    project editorHtml finished

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -5,10 +5,14 @@ public class Menu
     public static void Show()
     {
         Console.Clear();
-        Console.BackgroundColor = ConsoleColor.Blue;
+        //Console.BackgroundColor = ConsoleColor.Blue;
         Console.ForegroundColor = ConsoleColor.Black;
 
 		DrawScreen();
+		WriteOptions();
+
+		var option = short.Parse(Console.ReadLine());
+		HandleMenuOption(option);
         
         
     }
@@ -40,4 +44,40 @@ public class Menu
 		}
 		Console.Write("+");
 }
+
+	public static void WriteOptions()
+	{	
+		Console.SetCursorPosition(3,2);
+		Console.WriteLine("Editor HTML");
+		Console.SetCursorPosition(3,3);
+		Console.WriteLine("==========================");
+		Console.SetCursorPosition(3,4);
+		Console.WriteLine("Selecione uma opção abaixo");
+		Console.SetCursorPosition(3,6);
+		Console.WriteLine("1 - Novo arquivo");
+		Console.SetCursorPosition(3,7);
+		Console.WriteLine("2 - Abrir");
+		Console.SetCursorPosition(3,9);
+		Console.WriteLine("0 - Sair");
+		Console.SetCursorPosition(3,10);
+		Console.Write("Opção: ");
+		
+	}
+
+	public static void HandleMenuOption(short option)
+	{	
+		switch(option)
+		{
+			case 1: Editor.Show(); break;
+			case 2: Console.WriteLine("View"); break;
+			case 0: 
+				{
+					Console.Clear();
+					Environment.Exit(0);
+					break;
+				}
+
+			default: Show(); break;
+		}
+	}
 }
