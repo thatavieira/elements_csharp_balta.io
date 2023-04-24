1b09fea6d54e6945b1e13c92856b8c9cf5d2a739
thatavieira <thagurgel0909@gmail.com>
2023-04-24 19:50:16 -0300

    learning arrays

---
+++ Listas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+
+           var meuArray = new int[5] { 1, 2, 3, 4, 5 };
+           meuArray[0] = 12;
+
+           for (var index = 0; index <= meuArray.Length; index++)
+           {
+               Console.WriteLine((meuArray[index]));
+           }
+
+           foreach (var item in meuArray)
+           {
+               Console.WriteLine(item);
+           }
+           
+
+               
+          
+        }
+    }
+}
