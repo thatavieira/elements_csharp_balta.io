abb2505203a8ec23f39e9fea843bc38a07f22891
thatavieira <thagurgel0909@gmail.com>
2023-04-24 21:01:08 -0300

    learning exceeptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[3];
+
+            try
+            {
+                for (var index = 0; index < 10; index++)
+                {
+                    //IndexOutOfRangeException
+                    Console.WriteLine(arr[index]);
+                }
+
+                Cadastrar("testando");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Não encontrei o indice na lista");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Falha ao cadastrar texto");
+            }
+            catch (MinhaException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Ops, algo deu errado!");
+            }
+            finally
+            {
+                Console.WriteLine("Chegou ao fim!");
+            }
+        }
+
+        private static void Cadastrar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                throw new MinhaException(DateTime.Now);
+        }
+
+        public class MinhaException : Exception
+        {
+            public MinhaException(DateTime date)
+            {
+                QuandoAconteceu = date;
+            }
+
+            public DateTime QuandoAconteceu { get; set; }
+        }
+    }
+}
